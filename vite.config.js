08912ead657b87eec55ac54bcd89208c219988f1
edupetitvalley <edupetitvalley@users.noreply.github.com>
2026-01-03 04:48:54 +0100

    Update base path in Vite configuration
    Set the base path for the application.

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< Updated upstream
   base: '/eventPlanner/', // Important!
-=======
-  // base: '/your-repo-name/', // Important!
->>>>>>> Stashed changes
   plugins: [react()],
 })
